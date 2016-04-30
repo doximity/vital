0c06b89303127b365be1b65146e07b8b04fb83e2
Fabio Rehm <fgrehm@gmail.com>
2016-04-30 12:16:13 -0300

    Fix font compilation task

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,9 @@
-require 'middleman-gh-pages'
-
 namespace :vital do
   task :compile_fonts do
-    sh 'rm -rf source/stylesheets/_icons.*'
-    sh 'rm -rf source/fonts'
+    sh 'rm -rf assets/stylesheets/_icons.*'
+    sh 'rm -rf assets/fonts'
     sh 'fontcustom compile'
-    sh 'sass-convert source/stylesheets/_vital-icons.scss source/stylesheets/_icons.sass'
-    sh 'rm source/stylesheets/_vital-icons.scss'
+    sh 'sass-convert assets/stylesheets/_vital-icons.scss assets/stylesheets/vital/_icons.sass'
+    sh 'rm assets/stylesheets/_vital-icons.scss'
   end
 end
