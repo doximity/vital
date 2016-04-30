7a4206b0ac1f9b31e98beb58d0989a71ca8affcd
Fabio Rehm <fgrehm@gmail.com>
2016-04-30 11:43:14 -0300

    Introduce a rake task for compiling fonts and converting from scss to sass

--- rakefile.rb
+++ rakefile.rb
@@ -1,1 +1,11 @@
 require 'middleman-gh-pages'
+
+namespace :vital do
+  task :compile_fonts do
+    sh 'rm -rf source/stylesheets/_icons.*'
+    sh 'rm -rf source/fonts'
+    sh 'fontcustom compile'
+    sh 'sass-convert source/stylesheets/_vital-icons.scss source/stylesheets/_icons.sass'
+    sh 'rm source/stylesheets/_vital-icons.scss'
+  end
+end
