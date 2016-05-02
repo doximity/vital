33a286ff3e190ae5fb2cf12a4456dda27e67a668
Fabio Rehm <fgrehm@gmail.com>
2016-05-02 00:12:04 -0300

    Add fonts to rails assets path

--- lib/vital/engine.rb
+++ lib/vital/engine.rb
@@ -2,7 +2,9 @@ module Vital
   module Rails
     class Engine < ::Rails::Engine
       initializer 'vital.assets' do |app|
-        app.config.assets.paths << root.join('assets/stylesheets').to_s
+        %w(stylesheets fonts).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
       end
     end
   end
