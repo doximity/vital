ebef9d1907e9037b38a7e34f76ce0c5937b8ea49
Fabio Rehm <fgrehm@gmail.com>
2016-05-24 22:48:13 -0300

    Rake task for fonts, convert SCSS -> SASS, reorganize dirs

--- rakefile.rb
+++ rakefile.rb
@@ -1,1 +1,17 @@
 require 'middleman-gh-pages'
+
+namespace :vital do
+  task :compile_fonts do
+    sh 'rm -rf source/stylesheets/_icons.*'
+    sh 'rm -rf source/fonts'
+    sh 'fontcustom compile'
+    # Unquote escape sequences, e.g. content: "#{$sep}\00a0" to content: #{$sep}\00a0
+    # Works around Sass issue: https://github.com/sass/sass/issues/1395
+    sh 'sed -i.bkp \'s|"\(\\\\f[0-9a-z]*\)"|\1|g\' source/stylesheets/_icons.scss'
+    sh 'sass-convert source/stylesheets/_icons.scss source/stylesheets/_icons.sass'
+    # Add back the quote
+    sh 'sed -i.bkp \'s|\(\\\\f[0-9a-z]*\)|"\1"|g\' source/stylesheets/_icons.sass'
+    sh 'rm -f source/stylesheets/{,**/}*.bkp'
+    sh 'rm -f source/stylesheets/{,**/}*.scss'
+  end
+end
